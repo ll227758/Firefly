12b07141b8ca501f3d58cb22367b62afc5ed3093
ll227758 <3457330473@qq.com>
2026-08-14 09:31:31 +0800

    更新内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "aini",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "aini site",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
@@ -81,7 +81,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "aini",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
